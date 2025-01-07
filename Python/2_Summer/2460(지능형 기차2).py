6fae72e51120aebf28534955941137d79f2c71e2
jeonghoon11 <jih361@naver.com>
2025-01-07 18:29:57 +0900

    레포 이동

--- Python/2_Summer/2460(지능형 기차2).py
+++
@@ -1,9 +0,0 @@
-n, k = map(int, input().split())
-result = maxIn = k - n
-for _ in range(9):
-    n, k = map(int, input().split())
-    result = k - n + result
-    if result > maxIn:
-        maxIn = result
-
-print(maxIn)
